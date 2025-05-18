37d68d41551172abf157a1b70e06c51cd5608ae4
loveyChauhan <loveychauhan68363@gmail.com>
2025-05-18 05:49:36 +0530

    css fixed

--- src/component/Modal.jsx
+++ src/component/Modal.jsx
@@ -6,7 +6,7 @@ const Modal = ({ isOpen, onClose, children }) => {
     <>
       {isOpen && (
         <div className="absolute top-0 z-50 grid h-screen w-screen place-items-center backdrop-blur">
-          <div className="relative z-60 m-auto min-h-[200px] min-w-[90%] bg-white p-4">
+          <div className="relative z-60 m-auto min-h-[200px] w-full max-w-[90%] bg-white p-4 sm:max-w-[566px]">
             <div className="flex justify-end">
               <AiOutlineClose className="text-2xl" onClick={onClose} />
             </div>
